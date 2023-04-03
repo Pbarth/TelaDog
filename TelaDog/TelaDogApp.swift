35bd71dfa3884648fe73fe73407619fdebefa1f5
Pierre Barthelemy <p.barthelemy@digitsole.com>
2023-04-03 02:44:20 +0200

    Initial Commit

---
+++ TelaDog/TelaDogApp.swift
@@ -0,0 +1,17 @@
+//
+//  TelaDogApp.swift
+//  TelaDog
+//
+//  Created by Pierre BARTHELEMY on 03/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct TelaDogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TelaDogUITests/TelaDogUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  TelaDogUITestsLaunchTests.swift
+//  TelaDogUITests
+//
+//  Created by Pierre BARTHELEMY on 03/04/2023.
+//
+
+import XCTest
+
+final class TelaDogUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
